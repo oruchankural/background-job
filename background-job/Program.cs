2a43814f78430a14af34527a9dee21d06c68796c
oruchankural <oruchankural@gmail.com>
2023-10-08 14:10:02 +0300

    background-job example project structure for asp.net core v7

---
+++ background-job/Program.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Impl;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+// Configure and start the Quartz scheduler as a singleton service
+builder.Services.AddSingleton(provider =>
+{
+    var httpContextAccessor = provider.GetRequiredService<IHttpContextAccessor>();
+    var request = httpContextAccessor.HttpContext?.Request;
+    var baseUrl = $"{request?.Scheme}://{request?.Host}";
+
+    var schedulerFactory = new StdSchedulerFactory();
+    var scheduler = schedulerFactory.GetScheduler().Result;
+    scheduler.Start().Wait();
+
+    return scheduler;
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapPost("/api/data", async context =>
+{
+    var scheduler = context.RequestServices.GetRequiredService<IScheduler>();
+    var dataList = await context.Request.ReadFromJsonAsync<List<Data>>();
+
+    int batchSize = 100;
+    var totalDataCount = dataList.Count;
+    var jobCount = (int)Math.Ceiling((double)totalDataCount / batchSize);
+
+    for (int i = 0; i < jobCount; i++)
+    {
+        var batchData = dataList.Skip(i * batchSize).Take(batchSize).ToList();
+
+        var jobDataMap = new JobDataMap
+        {
+            { "BatchData", batchData }
+        };
+
+        var jobKey = new JobKey($"myJob_{i + 1}", "myGroup"); 
+
+        var jobDetail = JobBuilder.Create<InsertLogToDatabase>()
+            .WithIdentity(jobKey)
+            .UsingJobData(jobDataMap)
+            .Build();
+
+       // Start each job at different times, 2 minutes apart
+        var startTime = DateTime.Now.AddMinutes(2 * i);
+
+        var trigger = TriggerBuilder.Create()
+            .WithIdentity(jobKey.Name, jobKey.Group)
+            .StartAt(startTime)
+            .WithSimpleSchedule(x => x
+                .WithIntervalInMinutes(1))
+            .Build();
+
+        await scheduler.ScheduleJob(jobDetail, trigger);
+    }
+
+    context.Response.StatusCode = 200;
+    await context.Response.WriteAsync("Jobs started.");
+});
+
+app.Run();
+
+[DisallowConcurrentExecution]
+public class InsertLogToDatabase : IJob
+{
+    public Task Execute(IJobExecutionContext context)
+    {
+        var jobData = context.JobDetail.JobDataMap;
+        if (jobData.ContainsKey("BatchData") && jobData["BatchData"] is List<Data> batchData)
+        {
+            foreach (var data in batchData)
+            {
+                string logMessage = $"Data processed for job: {data.Id} - {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+                System.Console.WriteLine(logMessage);
+            }
+        }
+        return Task.CompletedTask;
+    }
+}
+
+public class Data
+{
+    public int Id { get; set; }
+    public string Message { get; set; }
+}
---
+++ background-job/readme.md
@@ -0,0 +1,44 @@
+Quartz.NET Scheduler with ASP.NET Core
+This is a basic example of using Quartz.NET as a scheduler in an ASP.NET Core application to process data in batches. It schedules jobs to run at different times, 2 minutes apart, and ensures that jobs of the same type do not run concurrently.
+
+Prerequisites
+.NET 5 or later
+Visual Studio or Visual Studio Code (optional)
+Getting Started
+Clone or download this repository.
+
+Open the project in Visual Studio or Visual Studio Code (optional).
+
+Run the application.
+
+Use a tool like Postman or curl to make a POST request to /api/data with a JSON array of Data objects to trigger the jobs.
+
+Project Structure
+Program.cs: Main entry point for the application.
+Startup.cs: Configuration of services and middleware.
+DataController.cs: Defines the API endpoint to trigger jobs.
+Jobs/InsertLogToDatabase.cs: The Quartz.NET job that processes data in batches.
+Models/Data.cs: A sample data model.
+appsettings.json: Configuration settings (not used in this example).
+requestBody.json : Request body for POST request
+
+Best Practices
+Use dependency injection to manage the Quartz scheduler as a singleton service.
+
+Avoid hardcoding values like job names or intervals. Use variables or configuration files for such values.
+
+Use JobKey to uniquely identify jobs. In this example, each job has a unique name with an index.
+
+Set a unique identity for each job and use it consistently when creating triggers.
+
+Start jobs at different times to avoid concurrent execution.
+
+Use [DisallowConcurrentExecution] to prevent jobs of the same type from running concurrently.
+
+Implement error handling and logging in your jobs as needed.
+
+Monitor and manage your Quartz.NET scheduler in a production environment.
+
+Keep the Startup.cs file clean by moving the scheduler configuration to a separate class or extension method for better maintainability.
+
+Feel free to customize and expand upon this example to meet your specific requirements.
